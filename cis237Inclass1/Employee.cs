ab45b8663524ad5487e6a777b150c3dd0b447a1e
Alyssa Mahler <amstrand21@gmail.com>
2016-09-13 16:52:06 -0400

    Add Employee class and some code to main.

---
+++ cis237Inclass1/Employee.cs
@@ -0,0 +1,67 @@
+﻿// Class to hold Employee information: FirstName, LastName, and WeeklySalary.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237Inclass1
+{
+    class Employee
+    {
+        // Backing Fields:
+        private string _firstName;
+        private string _lastName;
+        private decimal _weeklySalary;
+
+        // Properties:
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        public decimal WeeklySalary
+        {
+            get { return _weeklySalary; }
+            set { _weeklySalary = value; }
+        }
+
+        // Methods:
+        public override string ToString()   
+        // Overrides the implicit ToString to assign a particular set of values to the class name:
+        {
+            return this._firstName + " " + this.LastName;
+        }
+
+        public decimal YearlySalary()
+        {
+            return this.WeeklySalary * 52;
+        }
+
+        // Constructor(s):
+        public Employee()
+        {
+            // Default constructor.
+        }
+
+        public Employee(string firstName, string lastName, decimal weeklySalary)
+        {
+            this._firstName = firstName;
+            this._lastName = lastName;
+            this._weeklySalary = weeklySalary;
+
+
+        }
+
+
+
+
+
+    }
+}
--- cis237Inclass1/Program.cs
+++ cis237Inclass1/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Alyssa Strand (Mahler)
+// First In Class Program CIS 237
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +13,34 @@ namespace cis237Inclass1
     {
         static void Main(string[] args)
         {
+            Employee myEmployee = new Employee();
+
+            myEmployee.FirstName = "Alyssa";
+            myEmployee.LastName = "Strand";
+            myEmployee.WeeklySalary = 2000.00m;
+
+            Console.WriteLine(myEmployee.FirstName);
+            Console.WriteLine(myEmployee);
+
+            Employee[] employees = new Employee[10];
+
+            employees[0] = new Employee("James", "Kirk", 453m);
+            employees[1] = new Employee("Jessica", "Holmes", 700m);
+            employees[2] = new Employee("Alan", "Jones", 1000m);
+            employees[3] = new Employee("Tara", "Richards", 800m);
+            employees[4] = new Employee("Ella", "Fitzgerald", 520m);
+
+            foreach (Employee employee in employees)
+            {
+                if (employee != null)
+                {
+                    Console.WriteLine(employee.ToString() + " " + employee.YearlySalary());
+                }
+            }
+
+
+
+
         }
     }
 }
